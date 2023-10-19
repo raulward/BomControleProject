b02b4edd781e27ce0d35ed76b2c7ce89b315bb08
Raul Ward de Araujo <raulwarddev@gmail.com>
2023-10-19 06:37:27 -0300

    Changing vars name

--- tests.py
+++ tests.py
@@ -12,25 +12,25 @@ while True:
     password = login_data.getLoginPassword()
 
     # Instanciando a classe para verificar os dados de login
-    verify_user = UserLoginVerify(email, password)
+    verifyUser = UserLoginVerify(email, password)
     
-    email_result = verify_user.emailLoginVerify()
-    if email_result:
-        print(f'O endereço de e-mail "{email_result}" é válido.')
+    emailResult = verifyUser.emailLoginVerify()
+    if emailResult:
+        print(f'O endereço de e-mail "{emailResult}" é válido.')
     else:
         print(f'O endereço de e-mail "{email}" é inválido.')
         continue
    
-    password_result = verify_user.passwordLoginVerify()
+    passwordResult = verifyUser.passwordLoginVerify()
 
-    if password_result:
-        print(f'A senha "{password_result}" é válida.')
+    if passwordResult:
+        print(f'A senha "{passwordResult}" é válida.')
         break
     else:
         print(f'A senha é inválida.')
 
     # Pergunta se o usuário deseja tentar novamente
-    tentar_novamente = input("Deseja tentar novamente? (s/n): ")
-    if tentar_novamente.lower() != 's':
+    userOption = input("Deseja tentar novamente? (s/n): ")
+    if userOption.lower() != 's':
         break
 
